f4441e11ce22a1b1c2077d090f577c1d872d8da2
asura-hsuya <hiroryou4@gmail.com>
2025-01-30 15:56:10 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+let no=0 ;
+console.log(no)
+function toggleMenu() {
+   console.log('done')
+   
+    const div = document.getElementById("myDiv");
+    div.classList.toggle("-translate-x-full");
+    // div.classList.toggle("-z-40"); 
+  }
+
